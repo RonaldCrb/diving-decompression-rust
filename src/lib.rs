b1eb679d72e71df3cda58b3199114e4f1f888f83
Ron Alonzo <alonzo.ronald@gmail.com>
2020-06-26 13:04:25 +0200

    major refactor on crate structure, added extra checks to no_decompression_limit() and group_letter()

--- src/lib.rs
+++ src/lib.rs
@@ -61,160 +61,289 @@
 #[macro_use]
 
 extern crate serde_derive;
+
 /// this module provides functionality for the US Navy dive tables
 pub mod tables;
 
-pub fn no_decompression_limit(depth: u16) -> u16 {
-  //! Returns the no decompression limit for any given depth
-  //! depth must be expressed in Feet of sea water
-  //! No decompression limit is returned in minutes as u16 integer
-  
-  // no decompression table
-  let nodeco_table = tables::nodeco_table()
-    .expect("Error serializing the data withthe table deco_table");
-
-  let mut ndl: u16 = 0;  
-  for row in nodeco_table.table_data.iter() {
-    if row.min_fsw <= depth && depth <= row.max_fsw {
-      ndl = row.no_stop_limit
-    }
-  }
-  
-  return ndl;
+/// single dive object
+#[derive(Serialize, Deserialize, Copy, Clone, Debug)]
+pub struct Dive {
+  /// depth of the dive expressed in feet of sea water
+  pub depth: u16, 
+  /// bottom time of the dive expressed in minutes
+  pub bottom_time: u16,
 }
 
-pub fn group_letter(dive: tables::types::Dive) -> String {
-  //! Returns the group letter for a given dive. 
-  //! it takes a struct Dive as a parameter
-  //! the depth is expressed in feet of sea water
-  //! the bottom_time is expressed in minutes
-  //! the group letter is returned as a String
-  
-  // no decompression table
-  let nodeco_table = tables::nodeco_table()
-    .expect("Error serializing the data within the deco_table");
+/// single dive plan object
+#[derive(Serialize, Deserialize, Copy, Clone, Debug)]
+pub struct DivePlan {
+  /// the depth of the first planned dive expressed in feet of sea water
+  pub depth: u16,
+  /// the bottom time of the first planned dive expressed in minutes
+  pub bottom_time: u16,
+  /// the planned surface interval time expressed in minutes
+  pub surface_interval_time: u16,
+  /// the depth of the next planned dive expressed in feet of sea water
+  pub next_dive_depth: u16,
+}
 
-  let mut gl: String = String::from("");
-  for row in nodeco_table.table_data.iter() {
-    if row.min_fsw <= dive.depth && dive.depth <= row.max_fsw {
-      for value in row.values.iter() {
-        if value.min_time <= dive.bottom_time && dive.bottom_time <= value.max_time {
-          gl = String::from(&value.group_letter);
-        } 
+impl Dive {
+  pub fn new(depth: u16, bottom_time: u16) -> Self {
+    //! Instantiate a new Dive object 
+    Self {
+      depth,
+      bottom_time,
+    }
+  }
+
+  pub fn no_decompression_limit(self) -> u16 {
+    //! Returns the no decompression limit of the Dive Object up to a depth of 190 feet of sea water
+    //! No decompression limit is returned in minutes as u16 integer
+
+    let mut ndl: u16 = 0;  
+    
+    if self.depth > 190 {
+      return ndl;
+    } 
+
+    let nodeco_table = tables::nodeco_table()
+      .expect("Error serializing the data withthe table deco_table");
+
+    for row in nodeco_table.table_data.iter() {
+      if row.min_fsw <= self.depth && self.depth <= row.max_fsw {
+        ndl = row.no_stop_limit
       }
     }
+
+    return ndl;
   }
-  
-  return gl;
-}
 
-pub fn repet_letter(dive_plan: tables::types::DivePlan) -> String {
-  //! Returns the repetitive group letter for any given dive. 
-  //! it takes the dive profile as a paramater
-  //! the depth is expressed in feet of sea water
-  //! the time is expressed in minutes
-  //! the repetitive group letter is returned as a String
-  let nodeco_table = tables::nodeco_table()
-    .expect("Error serializing the data within the deco_table");
+  pub fn group_letter(self) -> String {
+    //! returns the group letter of the Dive object. 
+    //! the depth is expressed in feet of sea water
+    //! the bottom_time is expressed in minutes
+    //! the group letter is returned as a String
+    let mut gl: String = String::from("");
 
-  let rgl_table = tables::rgl_table()
-    .expect("there was an error deserializing deco table");
-  
-  let mut rl = String::new();
-  
-  for row in nodeco_table.table_data.iter() {
-    if row.min_fsw <= dive_plan.depth && dive_plan.depth <= row.max_fsw {
-      for group in row.values.iter() {
-        if group.min_time <= dive_plan.bottom_time && dive_plan.bottom_time <= group.max_time {
-          for rgl_row in rgl_table.table_data.iter() {
-            if rgl_row.group_letter == group.group_letter && rgl_row.min_time <= dive_plan.sit && dive_plan.sit <= rgl_row.max_time {
-              rl = String::from(&rgl_row.repet_letter)
-            }
+    let nodeco_table = tables::nodeco_table()
+      .expect("Error serializing the data within the deco_table");
+
+    for row in nodeco_table.table_data.iter() {
+      if row.min_fsw <= self.depth && self.depth <= row.max_fsw {
+        for value in row.values.iter() {
+          if value.min_time <= self.bottom_time && self.bottom_time <= value.max_time {
+            gl = String::from(&value.group_letter);
+          } 
+        }
+      }
+    }
+    
+    if gl == String::from("") && self.depth > 0 && self.depth <= 10 && self.bottom_time > 462 {
+      gl = String::from("F")
+    } else if gl == String::from("") && self.depth > 10 && self.depth <= 15 && self.bottom_time > 449 {
+      gl = String::from("I")
+    } else if gl == String::from("") && self.depth > 15 && self.depth <= 20 && self.bottom_time > 461 {
+      gl = String::from("L")
+    }
+
+    if gl == String::from("") && self.depth <= 190 {
+      gl = String::from("this dive is out of the time range for no-decompression air dives")
+    } else if gl == String::from("") && self.depth > 190 {
+      gl = String::from("this dive is out of the depth range for no-decompression air dives")
+    }
+
+    return gl;
+  }
+
+  pub fn deco_dive(self) -> tables::RowDeco {
+    //! returns the decompression profile of the Dive object. 
+    //! the depth is expressed in feet of sea water
+    //! the bottom_time is expressed in minutes
+    //! the decompression profile is returned as a RowDeco struct
+    let deco_table = tables::deco_table()
+      .expect("Error deserializing no decompression table");
+
+    let mut deco_profile: tables::RowDeco = tables::RowDeco {
+      min_time: 0,
+      max_time: 0,
+      air_tat: String::from("0"),
+      o2_tat: String::from("0"),
+      ttfs: String::from("0"),
+      o2cp: 0.0,
+      repetgroup_letter: String::from("0"),
+      surdo2_recommended: false,
+      exceptional_exposure: false,
+      surdo2_required: false,
+      strict_surdo2: false,
+      air_deco_stops: vec![],
+      o2_deco_stops: vec![],
+    };
+
+    for row_deco in deco_table.table_data.iter() {
+      if row_deco.min_fsw <= self.depth && self.depth <= row_deco.max_fsw  {
+        for profile in row_deco.rows.iter() {
+          if profile.min_time <= self.bottom_time && profile.max_time <= self.bottom_time {
+            deco_profile = profile.clone()
           }
-        } 
+        }
       }
     }
+    return deco_profile;
   }
-  
-  return rl;
 }
 
-pub fn residual_nitrogen_time(dive_plan: tables::types::DivePlan) -> u16 {
-  //! Returns the residual nitrogen time for a given dive plan. 
-  //! it takes the dive profile as a paramater
-  //! the depth is expressed in feet of sea water
-  //! the time is expressed in minutes
-  //! the residual nitrogen time is returned as a u16 integer
-  let nodeco_table = tables::nodeco_table()
-    .expect("Error deserializing no decompression table");
+impl DivePlan {
+  pub fn new(depth: u16, bottom_time: u16, surface_interval_time: u16, next_dive_depth: u16) -> Self {
+    //! Instantiates a new Dive Plan object
+    //! the depth and next_dive_depth are expressed in feet of sea water
+    //! the bottom_time and surface_interval_time are expressed in minutes
+    //! the returned object is of type DivePlan
+    Self {
+      depth,
+      bottom_time,
+      surface_interval_time,
+      next_dive_depth,
+    }
+  }
 
-  let rgl_table = tables::rgl_table()
-    .expect("Error deserializing repetitive group letter table");
+  pub fn from_dive(dive: Dive, surface_interval_time: u16, next_dive_depth: u16) -> Self {
+    //! Instantiates a new Dive Plan object from an existing Dive Object
+    //! the next_dive_depth is expressed in feet of sea water
+    //! the surface_interval_time is expressed in minutes
+    //! the returned object is of type DivePlan
+    Self {
+      depth: dive.depth,
+      bottom_time: dive.bottom_time,
+      surface_interval_time,
+      next_dive_depth,
+    }
+  }
 
-  let rnt_table = tables::rnt_table()
-    .expect("Error deserializing residual nitrogen time table");
+  pub fn no_decompression_limit(self) -> u16 {
+    //! returns the no decompression limit for the first dive of a DivePlan object 
+    //! No decompression limit is returned in minutes as u16 integer
+    let mut ndl: u16 = 0;  
+    
+    if self.depth > 190 {
+      return ndl;
+    } 
+
+    let nodeco_table = tables::nodeco_table()
+      .expect("Error serializing the data withthe table deco_table");
+
+    for row in nodeco_table.table_data.iter() {
+      if row.min_fsw <= self.depth && self.depth <= row.max_fsw {
+        ndl = row.no_stop_limit
+      }
+    }
+
+    return ndl;
+  }
+
+  pub fn group_letter(self) -> String {
+    //! returns the group letter of the first dive of a DivePlan object. 
+    //! the depth is expressed in feet of sea water
+    //! the bottom_time is expressed in minutes
+    //! the group letter is returned as a String
+    let mut gl: String = String::from("");
+
+    let nodeco_table = tables::nodeco_table()
+      .expect("Error serializing the data within the deco_table");
+
+    for row in nodeco_table.table_data.iter() {
+      if row.min_fsw <= self.depth && self.depth <= row.max_fsw {
+        for value in row.values.iter() {
+          if value.min_time <= self.bottom_time && self.bottom_time <= value.max_time {
+            gl = String::from(&value.group_letter);
+          } 
+        }
+      }
+    }
+    
+    if gl == String::from("") && self.depth > 0 && self.depth <= 10 && self.bottom_time > 462 {
+      gl = String::from("F")
+    } else if gl == String::from("") && self.depth > 10 && self.depth <= 15 && self.bottom_time > 449 {
+      gl = String::from("I")
+    } else if gl == String::from("") && self.depth > 15 && self.depth <= 20 && self.bottom_time > 461 {
+      gl = String::from("L")
+    }
+
+    if gl == String::from("") && self.depth <= 190 {
+      gl = String::from("this dive is out of the time range for no-decompression air dives")
+    } else if gl == String::from("") && self.depth > 190 {
+      gl = String::from("this dive is out of the depth range for no-decompression air dives")
+    }
+    return gl;
+  }
+
+  pub fn repet_letter(self) -> String {
+    //! Returns the repetitive group letter of the DivePlan object. 
+    //! the depth and next_dive_depth are expressed in feet of sea water
+    //! the bottom_time and surface_interval_time are expressed in minutes
+    //! the repetitive group letter is returned as a String
+    let nodeco_table = tables::nodeco_table()
+      .expect("Error serializing the data within the deco_table");
+
+    let rgl_table = tables::rgl_table()
+      .expect("there was an error deserializing deco table");
   
-  let mut rnt = 0;
+    let mut rl = String::new();
   
-  for row in nodeco_table.table_data.iter() {
-    if row.min_fsw <= dive_plan.depth && dive_plan.depth <= row.max_fsw {
-      for group in row.values.iter() {
-        if group.min_time <= dive_plan.bottom_time && dive_plan.bottom_time <= group.max_time {
-          for rgl_row in rgl_table.table_data.iter() {
-            if rgl_row.group_letter == group.group_letter && rgl_row.min_time <= dive_plan.sit && dive_plan.sit <= rgl_row.max_time {
-              for rnt_column in rnt_table.table_data.iter() {
-                if rnt_column.repet_letter == rgl_row.repet_letter {
-                  for element in rnt_column.rnt.iter() {
-                    if element.min_depth <= dive_plan.next_depth && dive_plan.next_depth <= element.max_depth {
-                      rnt = element.rnt
-                    } 
+    for row in nodeco_table.table_data.iter() {
+      if row.min_fsw <= self.depth && self.depth <= row.max_fsw {
+        for group in row.values.iter() {
+          if group.min_time <= self.bottom_time && self.bottom_time <= group.max_time {
+            for rgl_row in rgl_table.table_data.iter() {
+              if rgl_row.group_letter == group.group_letter && rgl_row.min_time <= self.surface_interval_time && self.surface_interval_time <= rgl_row.max_time {
+                rl = String::from(&rgl_row.repet_letter)
+              }
+            }
+          } 
+        }
+      }
+    }
+
+    return rl;
+  }
+
+  pub fn residual_nitrogen_time(self) -> u16 {
+    //! Returns the residual nitrogen time of the DivePlan object. 
+    //! the depth and next_dive_depth are expressed in feet of sea water
+    //! the bottom_time and surface_interval_time are expressed in minutes
+    //! the residual nitrogen time is returned as a u16 integer
+    let nodeco_table = tables::nodeco_table()
+      .expect("Error deserializing no decompression table");
+
+    let rgl_table = tables::rgl_table()
+      .expect("Error deserializing repetitive group letter table");
+
+    let rnt_table = tables::rnt_table()
+      .expect("Error deserializing residual nitrogen time table");
+  
+    let mut rnt = 0;
+    
+    for row in nodeco_table.table_data.iter() {
+      if row.min_fsw <= self.depth && self.depth <= row.max_fsw {
+        for group in row.values.iter() {
+          if group.min_time <= self.bottom_time && self.bottom_time <= group.max_time {
+            for rgl_row in rgl_table.table_data.iter() {
+              if rgl_row.group_letter == group.group_letter && rgl_row.min_time <= self.surface_interval_time && self.surface_interval_time <= rgl_row.max_time {
+                for rnt_column in rnt_table.table_data.iter() {
+                  if rnt_column.repet_letter == rgl_row.repet_letter {
+                    for element in rnt_column.rnt.iter() {
+                      if element.min_depth <= self.next_dive_depth && self.next_dive_depth <= element.max_depth {
+                        rnt = element.rnt
+                      } 
+                    }
                   }
                 }
               }
             }
-          }
-        } 
-      }
-    }
-  }
-  
-  return rnt;
-}
-
-pub fn deco_dive(dive: tables::types::Dive) -> tables::types::RowDeco {
-  //! Returns the decompression profile for a given dive. 
-  //! it takes a struct Dive as a parameter
-  //! the depth is expressed in feet of sea water
-  //! the bottom_time is expressed in minutes
-  //! the decompression profile is returned as a RowDeco struct
-  let deco_table = tables::deco_table()
-    .expect("Error deserializing no decompression table");
-
-  let mut deco_profile: tables::types::RowDeco = tables::types::RowDeco {
-    min_time: 0,
-    max_time: 0,
-    air_tat: String::from("0"),
-    o2_tat: String::from("0"),
-    ttfs: String::from("0"),
-    o2cp: 0.0,
-    repetgroup_letter: String::from("0"),
-    surdo2_recommended: false,
-    exceptional_exposure: false,
-    surdo2_required: false,
-    strict_surdo2: false,
-    air_deco_stops: vec![],
-    o2_deco_stops: vec![],
-  };
-
-  for row_deco in deco_table.table_data.iter() {
-    if row_deco.min_fsw <= dive.depth && dive.depth <= row_deco.max_fsw  {
-      for profile in row_deco.rows.iter() {
-        if profile.min_time <= dive.bottom_time && profile.max_time <= dive.bottom_time {
-          deco_profile = profile.clone()
+          } 
         }
       }
     }
+  
+    return rnt;
   }
-
-  return deco_profile;
 }
--- src/tables/mod.rs
+++ src/tables/mod.rs
@@ -1,13 +1,199 @@
 extern crate serde;
 extern crate serde_json;
 
-/// contains the types necesary to deserialize and serialize
-/// and operate with the US Navy dive tables
-pub mod types;
+/// A specific item in one of the rows of the US Navy air no-decompression table
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Group {
+  /// the repetitive group letter
+  pub group_letter: String,
+  /// the lower end of the timeframe for a specific row expressed in minutes
+  pub min_time: u16,
+  /// the higher end of the timeframe for a specific row expressed in minutes
+  pub max_time: u16,
+}
+
+/// row in the air No Decompression table
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct RowNdl {
+  /// the lower end of allowed depths for a specific
+  /// row expressed in Feet of sea water
+  pub min_fsw: u16,
+  /// the higher end of allowed depths for a specific 
+  /// row expressed in Feet of sea water
+  pub max_fsw: u16,
+  /// the row has unlimited no-decompression limit?
+  /// (usually no deeper than 30 Feet of sea water)
+  pub unlimited: bool,
+  /// the no decompression limit expressed in minutes
+  pub no_stop_limit: u16,
+  /// a specific item in one of the rows of the US Navy air no-decompression table
+  pub values: Vec::<Group>,
+}
+
+/// This is the main type for the entire US NAVY air No decompression table 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TableNdl {
+  /// unique id for a table within the diving-decompression project.
+  pub table_code: String,
+  /// oficially recognized name for a table.
+  pub table_name: String,
+  /// table data
+  pub table_data: Vec::<RowNdl>,
+}
+
+/// a row of the table for surface interval time and repetitive letter
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct RowRgl {
+  /// group letter before the surface interval time
+  pub group_letter: String,
+  /// the lower end of allowed times for a specific row expressed in minutes
+  pub min_time: u16,
+  /// the higher end of allowed times for a specific row expressed in minutes  
+  pub max_time: u16,
+  /// group letter after the surface interval time
+  pub repet_letter: String,
+}
+
+/// the table for surface interval time and repetitive letter
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TableRgl {
+  /// represents a unique id for a table within the 
+  /// diving-decompression project.
+  pub table_code: String,
+  /// oficially recognized name for a table beyond the
+  /// diving-decompression project.
+  pub table_name: String,
+  /// a row of the table for surface interval time and repetitive letter
+  pub table_data: Vec::<RowRgl>,
+}
+
+/// item in the row of the table for residual nitrogen time
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Rnt {
+  /// the lower end of allowed depth for a specific RNT item
+  /// expressed feet of sea water
+  pub min_depth: u16,
+  /// the higher end of allowed depth for a specific RNT item
+  /// expressed feet of sea water
+  pub max_depth: u16,
+  /// the residual nitrogen time expressed in minutes
+  pub rnt: u16,
+}
+
+/// a row in the table for residual nitrogen time
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct RowRnt {
+  /// the repet group letter for the residual nitrogen time item
+  pub repet_letter: String,
+  /// item in the row of the table for residual nitrogen time
+  pub rnt: Vec::<Rnt>,
+}
+
+/// the table for residual nitrogen time
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TableRnt {
+  /// represents a unique id for a table within the 
+  /// diving-decompression project.
+  pub table_code: String,
+  /// oficially recognized name for a table beyond the
+  /// diving-decompression project.
+  pub table_name: String,
+  /// a note displayed when the rnt exceeds the no 
+  /// decompression limit and the profile has an unlimited
+  /// no decompression limit in the no decompression table
+  /// for shallower depths as per the US Navy dive manual
+  pub table_note_9981: String,
+  /// a row in the table for residual nitrogen time
+  pub table_data: Vec::<RowRnt>,
+}
+
+/// an item from a row of the table for air decompression
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct DecoStops {
+  /// the nominal depth of a specific decompression stop
+  /// expressed in feet of sea water
+  pub depth: u16,
+  /// the nominal time of a specific decompression stop
+  /// expressed in minutes
+  pub time: u16,
+}
+
+/// a row in a depth of the air decompression
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct RowDeco {
+  /// the lower end of allowed times for a specific row expressed in minutes
+  pub min_time: u16,
+  /// the higher end of allowed times for a specific row expressed in minutes
+  pub max_time: u16,
+  /// total ascent time when using the Air based decompression protocol
+  /// expressed in minutes
+  pub air_tat: String,
+  /// total ascent time when using in water decompression with Oxygen
+  /// expressed in minutes
+  pub o2_tat: String,
+  /// the time from leave bottom to the first scheduled decompression stop
+  pub ttfs: String,
+  /// number of chamber periods when using the SurdO2 (surface decompression
+  /// with oxygen)
+  pub o2cp: f32,
+  /// repetitive dive group letter after the decompression protocol
+  pub repetgroup_letter: String,
+  /// SurdO2 is recommended due to the extent of the decompression profile
+  pub surdo2_recommended: bool,
+  /// exceptional exposure dives are considered an anti-pattern and should
+  /// only occur in extreme situations. planning a dive with exceptional
+  /// exposure is an anti-pattern and a tremendous risk for divers health
+  pub exceptional_exposure: bool,
+  /// surdO2 is required due to the extent of the decompression profile
+  pub surdo2_required: bool,
+  /// the dive must use the SurdO2 protocol. planning these dives with for
+  /// in water decompression is an anti-pattern and must be avoided
+  pub strict_surdo2: bool,
+  /// an air decompression stop
+  pub air_deco_stops: Vec::<DecoStops>,
+  /// an o2 decompression stop
+  pub o2_deco_stops: Vec::<DecoStops>,
+}
+
+/// a depth in the air decompression table
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct DecoDepth {
+  /// the lower end of allowed depth for a specific profile in the 
+  /// air decompression tables expressed feet of sea water
+  pub min_fsw: u16,
+  /// the higher end of allowed depth for a specific profile in the 
+  /// air decompression tables expressed feet of sea water
+  pub max_fsw: u16,
+  /// a row in a depth of the air decompression
+  pub rows: Vec::<RowDeco>,
+}
+
+/// the air decompression table
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TableAirDeco {
+  /// represents a unique id for a table within the 
+  /// diving-decompression project.
+  pub table_code: String,
+  /// oficially recognized name for a table beyond the
+  /// diving-decompression project.
+  pub table_name: String,
+  /// table data  
+  pub table_data: Vec::<DecoDepth>,
+}
 
 /// nodeco_table() returns a typed and serialized US Navy air 
 /// no-decompression table from rev7 of the US Navy dive manual. 
-pub fn nodeco_table() -> serde_json::Result<types::TableNdl> {
+pub fn nodeco_table() -> Result<TableNdl, String> {
+  let file = include_str!("JSON/usnavy-air-nodeco-rev7.json");
+  match serde_json::from_str(&file) {
+    Ok(v) => return v,
+    Err(e) => return Err(e.to_string()),
+  };
+}
+
+/// nodeco_table() returns a typed and serialized US Navy air 
+/// no-decompression table from rev7 of the US Navy dive manual.
+pub fn nodeco_table2() -> serde_json::Result<TableAirDeco> {
   let file = include_str!("JSON/usnavy-air-nodeco-rev7.json");
   let sertab = serde_json::from_str(&file); 
   return sertab;
@@ -15,7 +201,7 @@ pub fn nodeco_table() -> serde_json::Result<types::TableNdl> {
 
 /// deco_table() returns a typed and serialized US Navy air 
 /// decompression table from rev7 of the US Navy dive manual
-pub fn deco_table() -> serde_json::Result<types::TableAirDeco> {
+pub fn deco_table() -> serde_json::Result<TableAirDeco> {
   let file = include_str!("JSON/usnavy-air-deco-rev7.json");
   let sertab = serde_json::from_str(&file); 
   return sertab;
@@ -23,7 +209,7 @@ pub fn deco_table() -> serde_json::Result<types::TableAirDeco> {
 
 /// rgl_table() returns a typed and serialized US Navy repetitive group letter
 /// table from rev7 of the US Navy dive manual
-pub fn rgl_table() -> serde_json::Result<types::TableRgl> {
+pub fn rgl_table() -> serde_json::Result<TableRgl> {
   let file = include_str!("JSON/usnavy-air-repetgroup-rev7.json");
   let sertab = serde_json::from_str(&file); 
   return sertab;
@@ -31,7 +217,7 @@ pub fn rgl_table() -> serde_json::Result<types::TableRgl> {
 
 /// rnt_table() returns a typed and serialized US Navy residual nitrogen time
 /// table from rev7 of the US Navy dive manual
-pub fn rnt_table() -> serde_json::Result<types::TableRnt> {
+pub fn rnt_table() -> serde_json::Result<TableRnt> {
   let file = include_str!("JSON/usnavy-air-rnt-rev7.json");
   let sertab = serde_json::from_str(&file); 
   return sertab;
